1df93ef815d7c409f2c843aa8b77c716a69be9f9
cjsindt <connor-sindt@uiowa.edu>
2021-04-17 15:28:10 -0500

    coffee work

--- src/dependencies/coffee.py
+++ src/dependencies/coffee.py
@@ -3,8 +3,10 @@ import matplotlib.patches as mpatches
 import numpy as np
 
 
-def generate(matt = [1,1,1,1], connor = [2,2,2,2], jacob = [3,3,3,3], eric = [4,4,4,4]):
-    plt.plot(matt, 'r', connor, 'b', jacob, 'g', eric, 'y' )
+def generate(data):
+
+
+    #plt.plot(data[0], 'r', data[1], 'b', data[2], 'g', data[3], 'y' )
     matt_patch = mpatches.Patch(color = 'red', label = 'Matt')
     connor_patch = mpatches.Patch(color='blue', label='Connor')
     jacob_patch = mpatches.Patch(color='green', label='Jacob')
@@ -13,6 +15,5 @@ def generate(matt = [1,1,1,1], connor = [2,2,2,2], jacob = [3,3,3,3], eric = [4,
     plt.ylabel('Cups of Coffee')
     plt.legend(loc='upper left', handles = [matt_patch,connor_patch,jacob_patch,eric_patch])
     plt.savefig('..\static\coffee.png')
-    pass
 
 generate()
