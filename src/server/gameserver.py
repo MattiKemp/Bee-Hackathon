82c3e3ec414b12a44b783dd88772b915eeaa214c
root <root@DESKTOP-68UL55P.localdomain>
2021-04-18 11:50:07 -0500

    finished game

--- src/server/gameserver.py
+++ src/server/gameserver.py
@@ -1,7 +1,22 @@
 import math
 import random
+import csv
+import asyncio
 class gameserver:
     def __init__(self, connected):
+        # check credits for link to this data.
+        plasticData = []
+        with open('macroplastics-in-ocean.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line = 0
+            for row in csv_reader:
+                if line == 0:
+                    line += 1
+                plasticData.append([row["Year"], row["Accumulated ocean plastic: Macroplastics (>0.5cm)"]])
+            line += 1
+        #print(plasticData)
+        self.plasticData = plasticData
+        self.difficulty = 0
         # the info in each connected is: [x,y,size,xvel,yvel,food,garbage]
         self.connected = connected
         #self.globalData 
@@ -16,15 +31,30 @@ class gameserver:
             self.obstacles.add((random.randint(0,self.size),random.randint(0,self.size)))
         print(self.obstacles)
         self.trashpiles = set()
-        self.trashpileNum = 15
+        self.trashpileNum = 10 + self.difficulty//2
         while len(self.trashpiles) < self.trashpileNum:
             self.trashpiles.add((random.randint(0,self.size),random.randint(0,self.size)))
         self.update()
+        # link: https://stackoverflow.com/questions/37512182/how-can-i-periodically-execute-a-function-with-asyncio
+        async def periodic():
+            while True:
+                print('periodic')
+                self.difficulty+=1
+                self.trashpileNum = 10 + self.difficulty//2
+                while len(self.trashpiles) < self.trashpileNum:
+                    self.trashpiles.add((random.randint(0,self.size),random.randint(0,self.size)))
+                await asyncio.sleep(10)
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(periodic())
+        #try:
+        #    loop.run_until_complete(task)
+        #except asyncio.CancelledError:
+        #    pass
 
     def update(self):
         #print('updating')
         while len(self.food) < self.foodNum:
-            self.food.add((random.randint(0,self.size),random.randint(0,self.size),random.randint(0,10)>3))
+            self.food.add((random.randint(0,self.size),random.randint(0,self.size),random.randint(10,self.difficulty//2+10)<=25))
         toRemove = []
         # to tell if someone has picked up a food/trash
         for i in self.food:
@@ -44,7 +74,7 @@ class gameserver:
     def getData(self, user):
         # first index is characters, second is food, third is obstacles
         # the first index of characters is the users info
-        data = [[],[],[],[]]
+        data = [[],[],[],[],[]]
         died = False
         killed = False
         for i in self.connected:
@@ -85,16 +115,17 @@ class gameserver:
             if dist < 800:
                 data[2].append(i)
         if killed:
-            data[-1].append(user)
+            data[-2].append(user)
             #for i in range(4,len(data[-1])):
             #    if data[-1][i] < 0:
             #        data[-1][i] = 0
         elif not died:
-            data[-1].append(user)
-            data[-1][0][5] = 0
-            data[-1][0][6] = 0
+            data[-2].append(user)
+            data[-2][0][5] = 0
+            data[-2][0][6] = 0
         else:
-            data[-1].append([-1,-1,-1,-1,-1,-1,-1])
+            data[-2].append([-1,-1,-1,-1,-1,-1,-1])
+        data[-1].append([self.plasticData[self.difficulty][0],self.plasticData[self.difficulty][1]])
         return data
 
 def main():
